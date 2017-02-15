5428e82b469286942924746b12f48c46c9f17a00
TobiasHoefer <tobias.hoefer@outlook.de>
2017-02-15 16:01:28 +0100

    starting edge density

--- texture_features/main.cpp
+++ texture_features/main.cpp
@@ -100,11 +100,16 @@ int variance(Mat& src, int m, bool cuda_support){
     return 0;
 }
 
-int edge_density(Mat& src, int m, char& basis, bool cuda_support){
+int edge_density(Mat& src, int m, char basis, bool cuda_support){
     short unsigned int channel_count = src.channels();
     Mat chan[3];
+    unsigned int rows = src.rows;
+    unsigned int cols = src.cols;
+
+    
     //Apply Gaussian Blurr to reduce the noice
     GaussianBlur(src, src, Size(3,3), 0, 0, BORDER_DEFAULT);
+    display_img(src, "gaussian_blurr");
     //Sobel edge detection as basis
     if (basis == 'S'){
         int scale = 1;
@@ -118,77 +123,77 @@ int edge_density(Mat& src, int m, char& basis, bool cuda_support){
         int ddepth = CV_16S;
 
         /// Convert the image to grayscale
-        cvtColor( src, src_gray, CV_BGR2GRAY );
+        //cvtColor( src, src_gray, CV_BGR2GRAY );
 
     }
-    split(src, chan);
-
-    split(src, chan);
-    Mat output[3];
-    Mat merged_result;
-    
-    unsigned int rows = src.rows;
-    unsigned int cols = src.cols;
-    
-    //Number of pixel in a kernel m*m
-    int M = (float) (m * m);
-    
-    //iterate over all channels(B,G,R)
-    for (int c=0; c < channel_count; c++){
-        vector<uchar> result(rows*cols);
-        unsigned int r = 0;
-        //TODO:Delete vector ?
-        vector<uchar> environment(M);
-        unsigned int e;
-        double M_corner;
-        
-        //Iterating through bitmap of a single channel
-        for(int i=0; i < rows; i++){
-            for(int j=0; j < cols; j++){
-                
-                //kernel convolution for given size m
-                double mean = 0;
-                e = 0;
-                M_corner = M;
-                for(int x=i-(m-1)/2;x<=i+(m-1)/2;x++){
-                    for(int y=j-(m-1)/2;y<=j+(m-1)/2;y++){
-                        if (x >= 0 && y >= 0) {
-                            mean += (double)chan[c].at<uchar>(x,y);
-                            environment[e] = chan[c].at<uchar>(x,y);
-                        }else{
-                            environment[e] = -1;
-                            M_corner -= 1;
-                        }
-                        e++;
-                    }
-                }
-                mean = mean/M_corner;
-                //variance
-                double variance=0;
-                for(int l=0;l<M;l++){
-                    if(environment[l] >= 0){
-                        variance += pow(environment[l] - mean,2);
-                    }
-                }
-                double v = 1.0/((double)M_corner-1.0) * variance;
-                v = v > 255.0 ? 255.0 : v;
-                result[r] = v;
-                r++;
-            }
-        }
-        output[c] = Mat (rows,cols, CV_8UC1);
-        memcpy(output[c].data, result.data(), result.size()*sizeof(uchar));
-        result.clear();
-    }
-    display_img(output[0], "Blue");
-    display_img(output[1], "Green");
-    display_img(output[2], "Red");
-    vector<Mat> channels;
-    channels.push_back(output[0]);
-    channels.push_back(output[1]);
-    channels.push_back(output[2]);
-    merge(channels, merged_result);
-    display_img(merged_result, "RGB_Result");
+//    split(src, chan);
+//
+//    split(src, chan);
+//    Mat output[3];
+//    Mat merged_result;
+//    
+//    unsigned int rows = src.rows;
+//    unsigned int cols = src.cols;
+//    
+//    //Number of pixel in a kernel m*m
+//    int M = (float) (m * m);
+//    
+//    //iterate over all channels(B,G,R)
+//    for (int c=0; c < channel_count; c++){
+//        vector<uchar> result(rows*cols);
+//        unsigned int r = 0;
+//        //TODO:Delete vector ?
+//        vector<uchar> environment(M);
+//        unsigned int e;
+//        double M_corner;
+//        
+//        //Iterating through bitmap of a single channel
+//        for(int i=0; i < rows; i++){
+//            for(int j=0; j < cols; j++){
+//                
+//                //kernel convolution for given size m
+//                double mean = 0;
+//                e = 0;
+//                M_corner = M;
+//                for(int x=i-(m-1)/2;x<=i+(m-1)/2;x++){
+//                    for(int y=j-(m-1)/2;y<=j+(m-1)/2;y++){
+//                        if (x >= 0 && y >= 0) {
+//                            mean += (double)chan[c].at<uchar>(x,y);
+//                            environment[e] = chan[c].at<uchar>(x,y);
+//                        }else{
+//                            environment[e] = -1;
+//                            M_corner -= 1;
+//                        }
+//                        e++;
+//                    }
+//                }
+//                mean = mean/M_corner;
+//                //variance
+//                double variance=0;
+//                for(int l=0;l<M;l++){
+//                    if(environment[l] >= 0){
+//                        variance += pow(environment[l] - mean,2);
+//                    }
+//                }
+//                double v = 1.0/((double)M_corner-1.0) * variance;
+//                v = v > 255.0 ? 255.0 : v;
+//                result[r] = v;
+//                r++;
+//            }
+//        }
+//        output[c] = Mat (rows,cols, CV_8UC1);
+//        memcpy(output[c].data, result.data(), result.size()*sizeof(uchar));
+//        result.clear();
+//    }
+//    display_img(output[0], "Blue");
+//    display_img(output[1], "Green");
+//    display_img(output[2], "Red");
+//    vector<Mat> channels;
+//    channels.push_back(output[0]);
+//    channels.push_back(output[1]);
+//    channels.push_back(output[2]);
+//    merge(channels, merged_result);
+//    display_img(merged_result, "RGB_Result");
     return 0;
 }
 
@@ -201,6 +206,7 @@ int main(int argc, const char * argv[]) {
             system("pause");
             return -1;
         }
-    variance(img, 5, true);
+    //variance(img, 5, true);
+    edge_density(img, 3, 'L', false);
     return 0;
 }
